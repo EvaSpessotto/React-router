9229cac09a3fba4429b4c9d73c5739aaba48a349
Eva Spessotto <eva@spessotto.com>
2018-10-15 12:12:29 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
+import Home from './Components/Home';
+import OurHistory from './Components/OurHistory';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <nav>
+            <NavLink activeClassName="active" exact to="/">Home</NavLink> |&nbsp;
+            <NavLink activeClassName="active" to="/notre-histoire">Notre Histoire</NavLink>
+          </nav>
+
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/notre-histoire" component={OurHistory}/>
+          </Switch>
       </div>
     );
   }
